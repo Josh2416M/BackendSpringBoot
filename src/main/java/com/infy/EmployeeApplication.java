02ed7af5c82ac7ac4bdddd3ee6598d45b6a1f454
Josh2416M <142825675+Josh2416M@users.noreply.github.com>
2024-06-19 13:55:50 +0530

    Update EmployeeApplication.java

--- src/main/java/com/infy/EmployeeApplication.java
+++ src/main/java/com/infy/EmployeeApplication.java
@@ -4,10 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeApplication {
+public class EmployeeApplication implements commandLineRunner{
+
+	@Autowire
+	private App app;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		app.login();
 	}
 
 }
